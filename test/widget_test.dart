1c23ee09d0e4fb0f6db97d435e5094bdf072a0d5
Tahir786512 <m.tahirlearners123@gmail.com>
2025-02-18 18:23:44 +0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(const MyApp());
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+  // Wait for all animations to settle.
+  await tester.pumpAndSettle();
 
-import 'package:sololeveling/main.dart';
+  // Verify that our counter starts at 0.
+  expect(find.text('0'), findsOneWidget);
+  expect(find.text('1'), findsNothing);
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // Tap the '+' icon and trigger a frame.
+  await tester.tap(find.byIcon(Icons.add));
+  await tester.pump(); // Perform the frame update after the tap.
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  // Wait for the widget tree to settle again.
+  await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+  // Verify that our counter has incremented.
+  expect(find.text('0'), findsNothing);
+  expect(find.text('1'), findsOneWidget);
+});
